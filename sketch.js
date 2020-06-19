26715dcc47fdbe3a2d10baae6c43f5683e63c136
CraftinPark <craftinpark@gmail.com>
2020-06-19 18:38:14 -0400

    fixed image displaying, added tank movement through map, added turret - following cursor

--- sketch.js
+++ sketch.js
@@ -2,61 +2,94 @@ let tank;
 let ground;
 
 function preload() {
+  ground = createImg('https://cutewallpaper.org/21/minecraft-dirt-background/Images-of-Dirt-Background-Minecraft-wwwindustriousinfo.png');
+  ground.hide();
 }
 
 function setup() {
-  createCanvas(400, 400);
-  tank = new Tank(width/2, height/2, 20, 10);
+  createCanvas(800, 600);
+  tank = new Tank(width/2, height/2, 0, 2, 2);
+  map = new Map(width/2, height/2);
 }
 
 function draw() {
-  background(220);
-  fill(0);
-  rectMode(CENTER);
+
+  map.display();
   tank.display();
 
   //Movement keys
 
   // w key
   if (keyIsDown(87)) {
+    map.update(-sin(tank.a)*tank.dspeed,cos(tank.a)*tank.dspeed);
   }
   // s key
   if (keyIsDown(83)) {
+    map.update(sin(tank.a)*tank.dspeed,-cos(tank.a)*tank.dspeed);
   }
   //d key
-  if (keyIsDown(68)) {
-    tank.a += 1
-  }
+  if (keyIsDown(68))
+    tank.a += 1*tank.tspeed;
   //a key
-  if (keyIsDown(65)) {
-    tank.a -= 1
-  }
+  if (keyIsDown(65))
+    tank.a -= 1*tank.tspeed;
 }
 
-let isMoving = false;
-let direction = 0;
-
-
 class Tank {
-
-  constructor(x, y, a, speed) {
+  constructor(x, y, a, dspeed, tspeed) {
     this.a = a;
     this.x = x;
     this.y = y;
-    this.speed = speed;
-  }
-
-  move(x, y, a) {
-    this.a += a*this.speed;
-    this.x += x*this.speed;
-    this.y += y*this.speed;
+    this.dspeed = dspeed;
+    this.tspeed = tspeed;
   }
 
   display() {
+
+    //body setup
     push();
     translate(this.x,this.y);
-    rotate(radians(this.a));
-    rect(0, 0, 50, 50);
+    this.a = this.a % 360;
+
+    push();
+    rotate(this.a);
+
+    //body
+    rectMode(CENTER);
+    rect(0, 0, 60, 70);
+
+    //temporary info
+    textAlign(LEFT);
+    text(this.a, 30,0);
+    text('cos: ' + cos(radians(this.a)), 30,30);
+    text('sin: ' + sin(radians(this.a)), 30,40);
+
     pop();
+    
+    push();
+    //turret
+    angleMode(DEGREES);
+    rotate(atan2(mouseY - height / 2, mouseX - width / 2)+90);
+    ellipse(0,0,40,40);
+    rectMode(CENTER);
+    rect(0,-24,10,10);
+    pop();
+  }
+}
+
+class Map {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  update(x, y) {
+    this.x += x;
+    this.y += y;
+  }
+
+  display() {
+    imageMode(CENTER);
+    image(ground, this.x, this.y);
   }
 }
